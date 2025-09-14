fefd5c553897f62b156d4097b26f7ee63118daf4
重铬酸钠 <sthenight@outlook.com>
2025-09-14 15:16:02 +0800

    feat: 增加Installer和Teanslation子视图

---
+++ src/views/Installer.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { NResult, NButton } from 'naive-ui';
+import { useRouter } from 'vue-router';
+import Container from '@/components/Container.vue';
+const router = useRouter()
+</script>
+<template>
+    <div class="home-content-box">
+        <Container>
+            <div style="width: 100%;background-color: black;height: 100px;"></div>
+        </Container>
+    </div>
+</template>
+<style scoped lang="scss">
+.home-content-box {
+    box-sizing: border-box;
+    padding: 24px;
+}
+</style>
---
+++ src/views/Translation.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { NResult, NButton } from 'naive-ui';
+import { useRouter } from 'vue-router';
+const router = useRouter()
+</script>
+<template>
+    <div class="home-content-box">
+        建议你跳转到 <a href="https://sfs.izako.cc/">这里</a>，因为这个页面我还没写完
+    </div>
+</template>
+<style scoped lang="scss">
+.home-content-box {
+    box-sizing: border-box;
+    padding: 24px;
+}
+
+
+</style>
