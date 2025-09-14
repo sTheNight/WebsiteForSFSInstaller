981c6771108250477dff88c4bb9a90b7450add37
重铬酸钠 <sthenight@outlook.com>
2025-09-14 15:16:53 +0800

    feat: 增添主页卡片载入动画

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,31 +5,42 @@ const router = useRouter();
 </script>
 <template>
     <div class="home-content-box">
-        <NGradientText class="home-title-text" :gradient="{
-            deg: 90,
-            from: 'rgb( 60,195,183)',
-            to: 'rgb( 60,195,115)'
-        }">我也不知道我们叫什么名字</NGradientText>
-        <p class="home-subtitle-text">但反正我们可以给你提供如下的东西</p>
+        <Transition name="home-content-fade" appear>
+            <div class="home-title-content-box">
+                <NGradientText class="home-title-text" :gradient="{
+                    deg: 90,
+                    from: 'rgb( 60,195,183)',
+                    to: 'rgb( 60,195,115)'
+                }">我也不知道我们叫什么名字</NGradientText>
+                <p class="home-subtitle-text">但总之我们可以给你提供如下的东西</p>
+            </div>
+        </Transition>
         <div class="home-grid">
             <NGrid cols="1 500:2" x-gap="12" y-gap="12">
                 <NGridItem>
-                    <NCard hoverable title="SFS 汉化包" class="home-link-card">
-                        <template #action>
-                            <NButton>飞过去看看</NButton>
-                        </template>
-                        顾名思义
-                    </NCard>
+                    <Transition name="home-content-fade" appear style="transition-delay: 300ms;">
+                        <NCard title="SFS 汉化包" class="home-link-card">
+                            <template #action>
+                                <NButton @click="router.push('/translation')">飞过去看看</NButton>
+                            </template>
+                            顾名思义
+                        </NCard>
+                    </Transition>
                 </NGridItem>
                 <NGridItem>
-                    <NCard hoverable title="SFS 安装器" class="home-link-card">
-                        基于「原神」开发的闭源 SFS 安装器，应该可以安装汉化包和破解补丁，如果不行的话我也没办法，毕竟我说的是「应该」
-                        <template #action>
-                            <NButton @click="router.push('/installer')">飞过去看看</NButton>
-                        </template>
-                    </NCard>
+                    <Transition name="home-content-fade" appear style="transition-delay: 600ms;">
+                        <NCard title="SFS 安装器" class="home-link-card">
+                            基于「原神」开发的闭源 SFS 安装器，应该可以安装汉化包和破解补丁，如果不行的话我也没办法，毕竟我说的是「应该」
+                            <template #action>
+                                <NButton @click="router.push('/installer')">飞过去看看</NButton>
+                            </template>
+                        </NCard>
+                    </Transition>
                 </NGridItem>
             </NGrid>
+            <Transition name="home-content-fade" appear>
+                <p style="text-align: center;transition-delay: 900ms;font-size: 12px;color: gray;margin-top: 24px;">这一段动画很墨迹，对吧？</p>
+            </Transition>
         </div>
     </div>
 </template>
@@ -42,6 +53,12 @@ const router = useRouter();
     justify-content: center;
     align-items: center;
     padding: 24px;
+}
+
+.home-title-content-box {
+    padding: 48px 0;
+    display: flex;
+    flex-direction: column;
 }
 
 .home-title-text {
@@ -58,10 +75,30 @@ const router = useRouter();
 }
 
 .home-grid {
-    margin-top: 48px;
-    width: min(calc(100% - 24px), 700px);
+    width: min(100%, 800px);
 }
+
 .home-link-card {
     height: 100%;
+}
+</style>
+<style lang="scss">
+.home-content-fade-enter-active,
+.home-content-fade-leave-active {
+    transition: all .3s;
+}
+
+.home-content-fade-enter-from,
+.home-content-fade-leave-to {
+    opacity: 0;
+    transform: scale(1.2);
+    filter: blur(8px);
+}
+
+.home-content-fade-enter-to,
+.home-content-fade-leave-from {
+    opacity: 1;
+    transform: scale(1);
+    filter: blur(0px);
 }
 </style>
