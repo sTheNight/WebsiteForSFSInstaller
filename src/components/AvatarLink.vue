bb0e592f0a83832b817eac6b6a3d160c2bbd138e
重铬酸钠 <sthenight@outlook.com>
2025-09-15 19:37:55 +0800

    feat: 新增 AvatarLink 组件

---
+++ src/components/AvatarLink.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+const props = withDefaults(
+    defineProps<{
+        avatarLink: string;
+        name: string;
+        href: string;
+        leftPadding?: number;
+    }>(),
+    {
+        leftPadding: 2,
+    },
+);
+import { NElement, NAvatar } from "naive-ui";
+</script>
+
+<template>
+    <NElement
+        tag="a"
+        class="avatar-link"
+        :href="href"
+        :style="{ '--left-padding': `${leftPadding}px` }"
+    >
+        <NAvatar
+            class="user-avatar"
+            :size="18"
+            round
+            :src="avatarLink"
+        />
+        {{ name }}
+    </NElement>
+</template>
+
+<style lang="scss" scoped>
+.avatar-link {
+    display: inline-flex;
+    align-items: center;
+    vertical-align: middle;
+    gap: 4px;
+    color: var(--primary-color);
+    text-decoration: none;
+    padding: 0 6px 0 var(--left-padding);
+    border-radius: 50px;
+    border: 1px solid var(--n-border-color);
+    outline: 0;
+    transition: all 0.15s;
+
+    &:hover,
+    &:focus {
+        border-color: var(--primary-color);
+    }
+}
+</style>
